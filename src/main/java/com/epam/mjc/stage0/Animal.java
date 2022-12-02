a878a37e30955fd6ecd6827d892c67654d7c746f
DK <dasha.krutsko@gmail.com>
2022-12-02 13:17:20 +0300

    task1

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,5 +1,27 @@
 package com.epam.mjc.stage0;
 
 public class Animal {
+    private String colour;
+    private int numberOfPaws;
+    private boolean hasFur;
 
+    public static void main(String[] args) {
+        Dog jack =new Dog();
+        System.out.println(jack.getDescription());
+        Bird kesha = new Bird();
+        System.out.println(kesha.getDescription());
+    }
+
+    public Animal(String colour, int numberOfPaws, boolean hasFur) {
+        this.colour = colour;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+
+    public String getDescription(){
+        String hasFur = (this.hasFur) ? " a ": " no ";
+        String paw = (this.numberOfPaws>1)? " paws":" paw";
+        return "This animal is mostly " + this.colour + " It has "+ this.numberOfPaws + paw +" and" + hasFur + "fur.";
+    }
 }
+
