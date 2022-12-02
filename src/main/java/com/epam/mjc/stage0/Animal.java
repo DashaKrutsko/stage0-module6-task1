bdeb10b5f9c4d5b57a14a84388ef6a85d941e7aa
DK <dasha.krutsko@gmail.com>
2022-12-02 14:38:20 +0300

    task1

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -4,47 +4,10 @@ public class Animal {
     private String color;
     private int numberOfPaws;
     private boolean hasFur;
-//
-//    public static void main(String[] args) {
-//        Dog jack =new Dog();
-//        jack.setColour("brawn");
-//        jack.setNumberOfPaws(4);
-//        jack.setHasFur(true);
-//        System.out.println(jack.getDescription());
-//        Bird kesha = new Bird();
-//        kesha.setColour("blue");
-//        kesha.setNumberOfPaws(2);
-//        kesha.setHasFur(false);
-//        System.out.println(kesha.getDescription());
-//    }
 
     public Animal(String color, int numberOfPaws, boolean hasFur) {
         this.color = color;
         this.numberOfPaws = numberOfPaws;
-        this.hasFur = hasFur;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
-    }
-
-    public int getNumberOfPaws() {
-        return numberOfPaws;
-    }
-
-    public void setNumberOfPaws(int numberOfPaws) {
-        this.numberOfPaws = numberOfPaws;
-    }
-
-    public boolean isHasFur() {
-        return hasFur;
-    }
-
-    public void setHasFur(boolean hasFur) {
         this.hasFur = hasFur;
     }
 
